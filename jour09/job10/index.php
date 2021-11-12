ca7ee76145b1074ca23e4efec78261aefdecac33
ahmed sekou magassouba <ahmed.magassouba@laplateforme.io>
2021-11-12 12:31:43 +0100

    rr

---
+++ jour09/job10/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table {
+            margin: auto;
+            width: 80%;
+            border-collapse: collapse;
+            border: 1px solid black;
+            text-align: center;
+        }
+
+        th,
+        td {
+            border: 1px solid black;
+            height: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+
+    $bdd = mysqli_connect('localhost', 'root', '', 'jour08');
+
+    mysqli_set_charset($bdd, 'UTF8');
+
+    $requete = mysqli_query($bdd, 'SELECT * FROM salles ORDER BY capacite ');
+
+    $salles = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+
+   // var_dump($salles);
+
+    echo "<table><thead>
+    <th>id</th>
+    <th>nom</th>
+    <th>id_etage</th>
+    <th>capacite</th>
+
+   
+ </thead>
+<tbody>";
+        foreach ($salles as $salle) {
+            echo " <tr>
+               <td>" . $salle['id'] . "</td>
+               <td>" . $salle['nom'] . "</td>
+               <td>" . $salle['id_etage'] . "</td>
+               <td>" . $salle['capacite'] . "</td>
+
+           </tr>";
+      }
+
+        echo "</tbody>
+     </table>";
+
+
+
+
+    ?>
+
+</body>
+
+</html>
---
+++ jour09/job12/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table {
+            margin: auto;
+            width: 80%;
+            border-collapse: collapse;
+            border: 1px solid black;
+            text-align: center;
+        }
+
+        th,
+        td {
+            border: 1px solid black;
+            height: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+
+    $bdd = mysqli_connect('localhost', 'root', '', 'jour08');
+
+    mysqli_set_charset($bdd, 'UTF8');
+
+    $requete = mysqli_query($bdd, 'SELECT AVG(capacite) as capacite_moyenne FROM salles ');
+
+    $salles = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+
+   // var_dump($salles);
+
+    echo "<table><thead>
+  
+    <th>La capacite moyenne</th>
+
+   
+ </thead>
+<tbody>";
+        foreach ($salles as $salle) {
+            echo " <tr>
+               <td>" . $salle['capacite_moyenne'] . "</td>
+
+           </tr>";
+      }
+
+        echo "</tbody>
+     </table>";
+
+
+
+
+    ?>
+
+</body>
+
+</html>
